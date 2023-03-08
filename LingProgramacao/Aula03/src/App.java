7ebfcca942918a58530152dc66787371cb5588ec
douglasguedesdev <126295684+douglasguedesdev@users.noreply.github.com>
2023-03-07 21:41:54 -0300

    Add files via upload

---
+++ LingProgramacao/Aula03/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        ContaLogin cl1 = new ContaLogin();
+        ContaLogin cl2 = new ContaLogin();
+        ContaLogin cl3 = new ContaLogin();
+
+        cl1.setNome("Douglas");
+        cl2.setNome("DMG");
+        cl3.setNome("Manuel");
+
+        System.out.println("CL1: ");
+
+
+
+
+    }
+}
---
+++ LingProgramacao/Aula03/src/ContaCorrente.java
@@ -0,0 +1,23 @@
+public class DadosCliente {
+    private double saldo;
+
+    public ContaCorrente(String Titular){
+        this.titular = titular;
+
+
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    private String titular;
+
+
+
+
+}
